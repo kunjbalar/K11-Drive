c710bce6fa9fad51307985bed01e0e429abf3cb9
Kunj balar <kunjbalar44@gmail.com>
2026-03-02 17:20:21 +0530

    Initial commit

--- src/app/(home)/sign-in/page.tsx
+++ src/app/(home)/sign-in/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <>
       <SignInButton forceRedirectUrl={"/drive"} />
       <footer className="mt-16 text-sm text-neutral-500">
-        © {new Date().getFullYear()} T3 Drive. All rights reserved.
+        © {new Date().getFullYear()} K11 Drive. All rights reserved.
       </footer>
     </>
   );
